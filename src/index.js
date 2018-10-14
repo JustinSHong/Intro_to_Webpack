8e1af793e5e30914afea91042d4defa1c2ff517c
JustinSHong <jhong2009@gmail.com>
2018-10-14 11:39:29 -0700

    added icon.jpg to project and bundle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from "lodash";
 import "./style.css";
+import Icon from "./icon.jpg";
 
 function component() {
     let element = document.createElement("div");
 
+    // Lodash
     element.innerHTML = _.join(["Hello", "webpack"], " ");
     element.classList.add("hello");
+
+    // add the image to existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
